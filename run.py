7e0c6214ab903fc63bdbff636743152b37d129d2
Amelia Service <ekliwulj@students.codeinstitute.net>
2024-10-22 08:54:45 +0000

    made class and init function to initialize board with fixed size 8X8

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
 def get_user_name():
     """
-    Ask for the players name and return it
+    Ask for the users name and return it
     """
 print("Welcome to Battleship! \n")    
 user_name= input("Please enter your name: \n")
 print(f"Hello {user_name}, enjoy the game!!\n")
+    
+
+scores= {"user":0 , "computer":0}
+
+class Board:
+    
+    """ A class to represent the game board for Battleship-
+    The size of the board- initializes the boards with a fixed size of 8x8.
+    """
+
+    def __init__(self):
+        self.board_size= board_size
+        self.user_board= [['~' for _ in range(8)] for _ in range(8)]
+        self.compuer_board= [['~' for _ in range(8)] for _ in range(8)]
+        
+        
